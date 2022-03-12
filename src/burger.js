f0723bdce52cdcd7f9c8b3a983b227cdcaf73c04
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-03-12 18:00:20 +0200

    asdf

--- src/burger.js
+++ src/burger.js
@@ -1,3 +1,4 @@
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -6,3 +7,15 @@ function myFunction() {
       x.style.display = "block";
     }
 }
+
+const myLinks = document.querySelector('.menu-btn');
+let menuOpen = false;
+myLinks.addEventListener('click', () => {
+  if(!menuOpen) {
+    myLinks.classList.add('open');
+    menuOpen = true;
+  } else {
+    myLinks.classList.remove('open');
+    menuOpen = false;
+  }
+});
