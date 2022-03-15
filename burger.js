2a7b46a99a6508657a0620a1fed6a84cbc61eac3
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-03-15 23:17:15 +0200

    mm

--- burger.js
+++ burger.js
@@ -1,9 +1,17 @@
 
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+// Selection of HTML objects
+const burger = document.querySelector('.burger i');
+const nav = document.querySelector('.nav');
+
+// Defining a function
+function toggleNav() {
+    burger.classList.toggle('fa-bars');
+    burger.classList.toggle('fa-times');
+    nav.classList.toggle('nav-active');
 }
+
+// Calling the function after click event occurs
+burger.addEventListener('click', function() {
+    toggleNav();
+});
+
