57a211fa52ff63540355a33743b787dea83d4388
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-03-15 21:22:55 +0200

    ye

--- burger.js
+++ burger.js
@@ -7,15 +7,3 @@ function myFunction() {
       x.style.display = "block";
     }
 }
-
-const myLinks = document.querySelector('.menu-btn');
-let menuOpen = false;
-myLinks.addEventListener('click', () => {
-  if(!menuOpen) {
-    myLinks.classList.add('open');
-    menuOpen = true;
-  } else {
-    myLinks.classList.remove('open');
-    menuOpen = false;
-  }
-});
---
+++ scroll.js
@@ -0,0 +1,13 @@
+var lastScrollTop = 0;
+$(window).scroll(function(){
+  var st = $(this).scrollTop();
+  var banner = $('.banner');
+  setTimeout(function(){
+    if (st > lastScrollTop){
+      banner.addClass('hide');
+    } else {
+      banner.removeClass('hide');
+    }
+    lastScrollTop = st;
+  }, 100);
+});
